bc1ea3d92cdd4eef0a6cc951a7b4f5de7cea8fdc
Gustavovit02 <gusttavovitoriano@gmail.com>
2025-08-21 13:41:56 -0300

    Adicionando página functions e selecionando elementos DOM

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#edit-cancel-btn");
+const searchInput = document.querySelector("#search-input");
+const filterSelect = document.querySelector("#filter-select");
+
+let oldInputValue;
